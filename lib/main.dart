373e606322b02602220bf17a4ec40a52214a160b
mdarman925733-png <mdarman925733@gmail.com>
2026-08-10 01:46:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:provider/provider.dart';
 import 'core/localization/locale_provider.dart';
 import 'core/services/notification_service.dart';
@@ -24,6 +25,10 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance.activate(
+    providerAndroid: AndroidPlayIntegrityProvider(),
+    providerApple: AppleAppAttestProvider(),
+  );
   await NotificationService.initForegroundHandler();
   runApp(const GaonHaatApp());
 }
