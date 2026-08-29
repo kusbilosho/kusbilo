04b59e0230004262b9734d135e46541fe2e47880
kusbilosho <kusbilosho@gmail.com>
2026-08-29 09:42:28 +0530

    Add imageUrl to order items in checkout screen

--- lib/features/checkout/presentation/screens/checkout_screen.dart
+++ lib/features/checkout/presentation/screens/checkout_screen.dart
@@ -92,6 +92,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
               priceValue: entry.product!.priceValue,
               unit: entry.product!.unit,
               quantity: entry.qty,
+              imageUrl: entry.product!.imageUrl,
             ))
         .toList();
 
